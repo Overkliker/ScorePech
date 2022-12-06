c1e48342df8a3afef427f276d66b251c3880f0b4
Danila <danilahreno@gmail.com>
2022-12-06 11:18:11 +0300

    Добавьте файлы проекта.

---
+++ ScorePech/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorePech
+{
+    internal class User
+    {
+        public string username;
+        public int sInMin;
+        public double sInSec;
+    }
+}
